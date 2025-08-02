85eca00fbd218660be17eb3380ce79a20c6f32d9
Justin Bonus <bonus@berkeley.edu>
2025-08-01 18:57:21 -0700

    typo

--- Projects/OSU_LWF/read_scene_input.h
+++ Projects/OSU_LWF/read_scene_input.h
@@ -2957,7 +2957,7 @@ void parse_scene(std::string fn,
             }
 
             // Check for bathymetry coordinates (optional)
-            auto use_custom_bathymetry = CheckBool(geometry, "use_custom_bathymetry", false);
+            auto use_custom_bathymetry = CheckBool(model, "use_custom_bathymetry", false);
             if (use_custom_bathymetry || (object == "Bathymetry") || (object == "bathymetry")) {
               auto bathymetry = model.FindMember("bathymetry"); // [[x1,y1],[x2,y2],...]
               if (bathymetry != model.MemberEnd()) {
